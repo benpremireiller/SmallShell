b5c6ca1ef935e9fbfb5e064b02ee56707e973848
benpremireiller <benpremi@hotmail.com>
2025-02-16 19:12:30 -0500

    Added more signal handling logic

--- premireb_assignment4.c
+++ premireb_assignment4.c
@@ -13,6 +13,11 @@
 #define MAX_ARGS 512
 #define MAX_BG_COMMANDS 50
 
+bool delay_SIGTSTP = false;
+bool pending_SIGSTP = false;
+bool ignore_bg = false;
+
+
 
 struct command_line
 {
@@ -124,14 +129,17 @@ void reap_finished_bg_processes(int bg_processes[]){
         bg_pid = bg_processes[i];
 
         if (bg_pid != -1){ // a PID
-            bg_pid  = waitpid(bg_pid , &childExitStatus, WNOHANG);
-
-            if (bg_pid > 0){ // Process complete
+            bg_pid = waitpid(bg_pid , &childExitStatus, WNOHANG);
+            
+            if (WIFEXITED(childExitStatus)){
                 printf("background pid %d is done: exit value %d\n", bg_pid, childExitStatus);
                 fflush(stdout);
                 bg_processes[i] = -1;
 
-            }
+            } else if (WIFSIGNALED(childExitStatus)){
+                printf("background pid %d is done: terminated by signal %d", bg_pid, WTERMSIG(childExitStatus));
+                fflush(stdout);
+                bg_processes[i] = -1;
         }
     }
 
@@ -178,16 +186,31 @@ int redirect_stdout(char *destination){
 
 }
 
-void handle_SIGTSTP(int signo, int *ignore_bg){
-    char* message = "Terminated by signal \n";
-    write(STDOUT_FILENO, message, 25);
-    exit(0);
+void handle_SIGTSTP(int signo){ //Ctrl-Z
+    
+    if (delay_SIGTSTP){
+        pending_SIGSTP = true;
+        return;
+    }
+
+    if (!ignore_bg){
+        char* message = "Entering foreground-only mode (& is now ignored)\n";
+        write(STDOUT_FILENO, message, 49);
+        ignore_bg = true;
+
+    } else {
+        char* message = "Exiting foreground-only mode\n";
+        write(STDOUT_FILENO, message, 29);
+        ignore_bg = false;
+
+    }
+
+    pending_SIGSTP = false;
+    return;
 }
 
-void handle_SIGINT(int signo, int *ignore_bg){
-    char* message = "Terminated by signal \n";
-    write(STDOUT_FILENO, message, 25);
-    exit(0);
+void handle_SIGINT(int signo){ //Ctrl-C
+    exit(signo); // This right??
 }
 
 
@@ -201,26 +224,27 @@ int main(){
     int sourceFD;
     int bg_processes[MAX_BG_COMMANDS] = {-1}; // Change this type to pid_t?
     int bg_process_cnt;
-    int ignore_bg = false;
 
     // Set up signal handlers
-    struct sigaction SIGSTP_action = {0}, ignore_action = {0};
+    struct sigaction SIGTSTP_action = {0}, ignore_action = {0};
     
-    // Handle SIGINT
-    SIGSTP_action.sa_handler = handle_SIGINT;
+    // Handle SIGTSTP
+    SIGTSTP_action.sa_handler = handle_SIGTSTP; 
+    sigaction(SIGTSTP, &SIGTSTP_action, NULL);
+    sigfillset(&SIGTSTP_action.sa_mask); // Block all signals while running
 
     // Ignore SIGINT
     ignore_action.sa_handler = SIG_IGN;
     sigaction(SIGINT, &ignore_action, NULL);
 
-    
-
-    sigaction(SIGINT, &ignore_action, NULL);
-
     while(true)
     {
-
+        
+        // Delay SIGTSTP signal until after execution of command, may need to move these around fgets() in parse_input()
+        delay_SIGTSTP = false;
         curr_command = parse_input();
+        delay_SIGTSTP = true;
+
         //print_command_line_struct(curr_command);
 
         // Handle built-in command first
@@ -240,16 +264,38 @@ int main(){
         } else { // Non built-in command. 
             spawnpid = fork();
             switch (spawnpid){
+
                 case -1:
                     perror("fork() failed!");
                     exit(1);
 
                 case 0: // Child
                     
-                    // Ignore SIGSTP/SIGTSTP for all children. Maybe I can reuse the parent sigaction struct and re register that way?
-                    struct sigaction ignore_action = {0};
-                    ignore_action.sa_handler = SIG_IGN;
+                    // Ignore SIGTSTP for all children. 
                     sigaction(SIGTSTP, &ignore_action, NULL);
+
+                    // Handle SIGINT
+                    struct sigaction SIGINT_action = {0};
+                    SIGINT_action.handler = handle_SIGINT;
+                    sigaction(SIGINT, &SIGINT_action, NULL);
+
+                    if (curr_command->is_bg && !ignore_bg){
+
+                        // Background processes ignore SIGINT
+                        sigaction(SIGINT, &ignore_action, NULL);
+
+                        printf("background pid is %d\n", getpid());
+                        fflush(stdout);
+
+                        // Redirect bg process streams to /dev/null if there is no value specified 
+                        if(!curr_command->input_file && redirect_stdin("/dev/null") == -1){
+                            exit(2);
+                        }
+
+                        if(!curr_command->output_file && redirect_stdout("/dev/null") == -1){
+                            exit(2);
+                        }
+                    }
 
                     // Redirect streams if necessary
                     if(curr_command->input_file && redirect_stdin(curr_command->input_file) == -1){
@@ -260,20 +306,6 @@ int main(){
                         exit(2);
                     }
 
-                    if (curr_command->is_bg && !ignore_bg){
-                        printf("background pid is %d\n", getpid());
-                        fflush(stdout);
-
-                        // Redirect bg process streams if there is no value specified, redirect to /dev/null
-                        if(!curr_command->input_file && redirect_stdin("/dev/null") == -1){
-                            exit(2);
-                        }
-
-                        if(!curr_command->output_file && redirect_stdout("/dev/null") == -1){
-                            exit(2);
-                        }
-                    }
-
                     // Run command with exec. Search PATH and execute it in child process.
                     (curr_command->argv)[(curr_command->argc)+1] = NULL; // Add null terminator to args
                     execvp(curr_command->command, curr_command->argv);
@@ -282,12 +314,19 @@ int main(){
         
                 default: // Parent
                     
-                    if(!curr_command->is_bg){
+                    if(!curr_command->is_bg || ignore_bg){
                         spawnpid = waitpid(spawnpid, &childExitStatus, 0);
+
                         if (WIFEXITED(childExitStatus)){
-                            status = 0; // TODO, this is returning 0 even if I send a bogus command to execvp
+                            status = 0; // TODO, this is returning 0 even if I send a bogus command to execvp.
+
+                        } else if (WIFSIGNALED(childExitStatus)){
+                            printf("terminated by signal %d", WTERMSIG(childExitStatus));
+                            fflush(stdout);
+
                         } else {
                             status = 1;
+
                         }
 
                     } else {
@@ -296,6 +335,11 @@ int main(){
                     }
                     break;
             }
+        }
+
+        // Send SIGTSTP to current process if pending
+        if (pending_SIGSTP){
+            kill(getpid(), SIGTSTP);
         }
 
         // Check for finished background commands
